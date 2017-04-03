bbdf95d62eeacc9eaa94eb9d12882131122aa2cd
Sean Cali <seankenjicali@gmail.com>
2017-04-03 15:20:22 -0700

    Final API

--- VehicleManager.API/App_Start/WebApiConfig.cs
+++ VehicleManager.API/App_Start/WebApiConfig.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using Newtonsoft.Json.Serialization;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace VehicleManager.API
 {
@@ -19,6 +21,13 @@ namespace VehicleManager.API
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+            config.EnableCors(new EnableCorsAttribute("*", "*", "*"));
+
+            config.Formatters.JsonFormatter.SerializerSettings.ContractResolver =
+                new CamelCasePropertyNamesContractResolver();
+
+            config.Formatters.Remove(config.Formatters.XmlFormatter);
         }
     }
 }
---
+++ VehicleManager.API/Data/VehicleManagerDataContext.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using VehicleManager.API.Models;
+
+namespace VehicleManager.API.Data
+{
+    public class VehicleManagerDataContext : DbContext
+    {
+        // Constructor
+        public VehicleManagerDataContext() : base("VehicleManager")
+        {
+
+        }
+        // DbSets
+        public IDbSet<Customer> Customers { get; set; }
+        public IDbSet<Sale> Sales { get; set; }
+        public IDbSet<Vehicle> Vehicles { get; set; }
+        // Model Configuration
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            // Vehicle has many sales
+            modelBuilder.Entity<Vehicle>()
+                        .HasMany(vehicle => vehicle.Sales)
+                        .WithRequired(sale => sale.Vehicle)
+                        .HasForeignKey(sale => sale.VehicleId);
+
+            // Customer has many sales
+            modelBuilder.Entity<Customer>()
+                        .HasMany(customer => customer.Sales)
+                        .WithRequired(sale => sale.Customer)
+                        .HasForeignKey(sale => sale.CustomerId);
+        }
+    }
+}
