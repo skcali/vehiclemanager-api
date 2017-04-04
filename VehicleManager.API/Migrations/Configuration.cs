931d5d6143aa0d0c81ed32ddeb46cbaadba18b15
Sean Cali <seankenjicali@gmail.com>
2017-04-04 16:28:31 -0700

    Added Faker

--- VehicleManager.API/Migrations/Configuration.cs
+++ VehicleManager.API/Migrations/Configuration.cs
@@ -14,18 +14,22 @@ namespace VehicleManager.API.Migrations
 
         protected override void Seed(VehicleManager.API.Data.VehicleManagerDataContext context)
         {
-            //  This method will be called after migrating to the latest version.
-
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data. E.g.
-            //
-            //    context.People.AddOrUpdate(
-            //      p => p.FullName,
-            //      new Person { FullName = "Andrew Peters" },
-            //      new Person { FullName = "Brice Lambson" },
-            //      new Person { FullName = "Rowan Miller" }
-            //    );
-            //
+            if(context.Customers.Count() == 0)
+            {
+                for (int i = 0; i < 20; i++)
+                {
+                    context.Customers.Add(new Models.Customer
+                    {
+                        EmailAddress = Faker.InternetFaker.Email(),
+                        DateOfBirth = Faker.DateTimeFaker.BirthDay(),
+                        FirstName = Faker.NameFaker.FirstName(),
+                        LastName = Faker.NameFaker.LastName(),
+                        Telephone = Faker.PhoneFaker.Phone()
+                    });
+                }
+                context.SaveChanges();
+            }
+            
         }
     }
 }
